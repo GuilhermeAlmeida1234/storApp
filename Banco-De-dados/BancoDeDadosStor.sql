f087f5391eea48b125929586fdf712abdbd1b0f2
Guilherme Almeida <guilherme.asantos126@senacsp.edu.br>
2026-05-07 16:44:17 -0300

    Banco de dados finalizado

--- Banco-De-dados/BancoDeDadosStor.sql
+++ Banco-De-dados/BancoDeDadosStor.sql
@@ -1,28 +1,47 @@
 CREATE DATABASE StorApp
 USE StorApp
-Create table Tbl_Prestador (
- id_Prestador INT IDENTITY PRIMARY KEY,
- Nome_Prestador Varchar (70),
- CPF_Prestador CHAR (11),
- Telefone_Prestador Varchar (15),
- Email_Prestador Varchar (50),
- Endereço_Prestador Varchar(50)
+
+CREATE TABLE tbl_prestador (
+	IDPrestador INT IDENTITY PRIMARY KEY,
+	nomePrestador VARCHAR(50),
+	profissao VARCHAR(50),
+	CPFPrestador CHAR(11),
+	telefonePrestador VARCHAR (15),
+	emailPrestador VARCHAR (50),
+	rua VARCHAR(50),
+	bairro VARCHAR(50),
+	cidade VARCHAR(50),
+	estado VARCHAR(20),
+	UF CHAR(2),
+	pontoDeReferencia VARCHAR(100)
 )
 
-Create table Tbl_Contratnte (
- id_Contrante INT IDENTITY PRIMARY KEY,
- Nome_Contratante Varchar (70),
- CPF_Contratante CHAR (11),
- Telefone_Contratante Varchar (15),
- Email_Contratante Varchar (50),
- Endereço_Contratante Varchar(50)
+CREATE TABLE tbl_contratante (
+	IDContratante INT IDENTITY PRIMARY KEY,
+	nomeContratante VARCHAR(70),
+	CPFContratante CHAR(11),
+	telefoneContratante VARCHAR(15),
+	emailContratante VARCHAR(50),
+	rua VARCHAR(100),
+	bairro VARCHAR(100),
+	cidade VARCHAR(100),
+	estado VARCHAR(100),
+	UF CHAR(2),
+	pontoDeReferencia VARCHAR(100)
 )
 
-create table Serviços(
-id_ServiçoPrestado INT IDENTITY PRIMARY KEY,
-ID_prestador INT,
-data_Hora DATETIME,
-valor_Serviço DECIMAL (7,2),
+CREATE TABLE tbl_serviços(
+	IDServico INT IDENTITY PRIMARY KEY,
+	dataHora DATETIME,
+	valorServiço DECIMAL(7,2),
+)
+
+CREATE TABLE tbl_servicoRealizado(
+	ID INT IDENTITY PRIMARY KEY,
+	IDPrestador INT,
+	IDContratante INT,
+	FOREIGN KEY (IDPrestador) REFERENCES tbl_prestador (IDPrestador),
+	FOREIGN KEY (IDContratante) REFERENCES tbl_contratante (IDContratante)
 )
 
 
