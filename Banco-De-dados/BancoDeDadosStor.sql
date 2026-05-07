813435eb06883fa086ae46a173306572b4ec7d2c
Nicollas Candido <nicollas.clima@senacsp.edu.br>
2026-05-07 16:15:48 -0300

    Salvando banco de dados

--- Banco-De-dados/BancoDeDadosStor.sql
+++ Banco-De-dados/BancoDeDadosStor.sql
@@ -1,5 +1,28 @@
 CREATE DATABASE StorApp
 
-Create table 
+Create table Tbl_Prestador (
+ id_Prestador INT IDENTITY PRIMARY KEY,
+ Nome_Prestador Varchar (70),
+ CPF_Prestador CHAR (11),
+ Telefone_Prestador Varchar (15),
+ Email_Prestador Varchar (50),
+ Endereço_Prestador Varchar(50)
+)
+
+Create table Tbl_Contratnte (
+ id_Contrante INT IDENTITY PRIMARY KEY,
+ Nome_Contratante Varchar (70),
+ CPF_Contratante CHAR (11),
+ Telefone_Contratante Varchar (15),
+ Email_Contratante Varchar (50),
+ Endereço_Contratante Varchar(50)
+)
+
+create table Serviços(
+id_ServiçoPrestado INT IDENTITY PRIMARY KEY,
+ID_prestador INT,
+data_Hora DATETIME,
+valor_Serviço DECIMAL (7,2),
+)
 
 
